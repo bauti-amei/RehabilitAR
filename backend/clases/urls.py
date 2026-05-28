9079f2d616686bfc32fdd38ceb52a69c9a2802e5
Bautista Ameijeiras <bautista.ameijeiras@gmail.com>
2026-05-27 21:32:03 -0300

    fix: agrego url faltante para desasignarse de clase

--- backend/clases/urls.py
+++ backend/clases/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path('mis-clases/',                MisClasesView.as_view(),                 name='mis-clases'),
     path('ofertadas/',                 ClasesOfertadasView.as_view(),            name='clases-ofertadas'),
     path('<int:pk>/asignarse/',        AsignarseClaseView.as_view(),             name='clase-asignarse'),
+    path('<int:pk>/desasignarse/',     DesasignarseClaseView.as_view(),          name='clase-desasignarse'),
     path('<int:pk>/asignar-profesor/', AsignarProfesorView.as_view(),            name='clase-asignar-profesor'),
     path('salas/',                     SalaListCreateView.as_view(),             name='sala-list-create'),
     path('profesores/',                ProfesoresPorEspecialidadView.as_view(),  name='profesores-especialidad'),
